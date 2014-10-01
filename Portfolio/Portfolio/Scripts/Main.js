264cad38cb7e08f8a7d88bf4a5198f3996d9e8e0
SeedPaths-User <lessdismissive@email.com>
2014-10-01 11:33:21 -0600

    Added Carousel

---
+++ Portfolio/Portfolio/Scripts/Main.js
@@ -0,0 +1,15 @@
+﻿//load the DOM into jQueryy
+$(document).ready(function () {
+  
+        $('.image-next').on('click', function () {
+            var activeSlide = $('.active');
+            var nextSlide = activeSlide.next();
+            //make sure its a carousel slide
+            if (!nextSlide.hasClass('hide')) {
+              
+                nextSlide = $('.active').first();
+            }
+            activeSlide.removeClass('active').addClass('hide');
+            nextSlide.removeClass('hide').addClass('active');
+    });
+});
